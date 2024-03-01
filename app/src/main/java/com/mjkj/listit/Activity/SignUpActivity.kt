c05e6cc311c476c5ca1b0dc2f6c1c205a6a29f7c
Marcin Kawa <lerukcja2002@gmail.com>
2024-03-01 21:11:56 +0100

    basic activity look

--- app/src/main/java/com/mjkj/listit/Activity/SignUpActivity.kt
+++ app/src/main/java/com/mjkj/listit/Activity/SignUpActivity.kt
@@ -3,46 +3,53 @@ package com.mjkj.listit.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            Surface (modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background){
-                Column {
-
-
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-
-                        Text(
-                            text = "List It",
-                            style = MaterialTheme.typography.displayLarge,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                    }
-                }
+            Surface(modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background) {
+                SignUpContent()
             }
+        }
+    }
+}
+
+@Composable
+fun SignUpContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Enter your name") },
+            modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
+        )
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Enter your email") },
+            modifier = Modifier.padding(10.dp)
+        )
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Enter your password") },
+            modifier = Modifier.padding(top = 10.dp, bottom = 150.dp)
+        )
+
+        ButtonTonalFilled("Sign Up") {
         }
     }
 }
