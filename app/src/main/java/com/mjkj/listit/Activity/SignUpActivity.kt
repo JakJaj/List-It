b82333fa5fb02b50b2fe82ea446dddd6301cd17c
Marcin Kawa <lerukcja2002@gmail.com>
2024-03-01 21:52:17 +0100

    fix register layout

--- app/src/main/java/com/mjkj/listit/Activity/SignUpActivity.kt
+++ app/src/main/java/com/mjkj/listit/Activity/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.mjkj.listit.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,46 +11,45 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mjkj.listit.Composable.*
 
 class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(modifier = Modifier.fillMaxSize(),
+
+            Surface(
+                modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background) {
-                SignUpContent()
+
+                Column {
+
+                    Column (modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center){
+
+                        OutlinedTextField("Username")
+                        Spacer(modifier = Modifier.padding(10.dp))
+                        OutlinedTextField("Email")
+                        Spacer(modifier = Modifier.padding(10.dp))
+                        OutlinedTextField("Password")
+                        Spacer(modifier = Modifier.padding(10.dp))
+                        OutlinedTextField("Confirm password")
+
+                        Spacer(modifier = Modifier.padding(60.dp))
+
+                        ButtonFilled("Sign Up") {
+                            //TODO: Handle registration
+                        }
+                        Spacer(modifier = Modifier.padding(5.dp))
+                        ButtonTonalFilled(label = "Go back"){
+                            //TODO: Handle going back to main screen
+                            val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                }
             }
-        }
-    }
-}
-
-@Composable
-fun SignUpContent() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        TextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Enter your name") },
-            modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
-        )
-        TextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Enter your email") },
-            modifier = Modifier.padding(10.dp)
-        )
-        TextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Enter your password") },
-            modifier = Modifier.padding(top = 10.dp, bottom = 150.dp)
-        )
-
-        ButtonTonalFilled("Sign Up") {
         }
     }
 }
