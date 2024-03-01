b06a534c67389a851270762e9c3fc53faf9d5360
Marcin Kawa <lerukcja2002@gmail.com>
2024-03-01 21:39:12 +0100

    fix conflict

---
+++ app/src/main/java/com/mjkj/listit/Activity/LogInActivity.kt
@@ -0,0 +1,88 @@
+package com.mjkj.listit.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mjkj.listit.Composable.*
+
+class LogInActivity: ComponentActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background) {
+
+                Column {
+
+                    Column (modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center){
+
+                        OutlinedTextField("Username")
+                        Spacer(modifier = Modifier.padding(10.dp))
+                        OutlinedTextField("Password")
+
+                        Spacer(modifier = Modifier.padding(60.dp))
+
+                        ButtonFilled("Log in") {
+                            //TODO: Handle login
+                        }
+                        Spacer(modifier = Modifier.padding(5.dp))
+                        ButtonTonalFilled(label = "Go back"){
+                            //TODO: Handle going back to main screen
+                            val intent = Intent(this@LogInActivity, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background) {
+
+        Column {
+
+            Column (modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center){
+
+                OutlinedTextField("Username")
+                Spacer(modifier = Modifier.padding(10.dp))
+                OutlinedTextField("Password")
+
+                Spacer(modifier = Modifier.padding(60.dp))
+
+                ButtonFilled("Log in") {
+                    //TODO: Handle login
+                }
+                ButtonTonalFilled(label = "Go back"){
+                    //TODO: Handle going back to main screen
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/mjkj/listit/Activity/MainActivity.kt
+++ app/src/main/java/com/mjkj/listit/Activity/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.mjkj.listit.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,37 +18,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import android.content.Intent
+import com.mjkj.listit.Composable.ButtonFilled
+import com.mjkj.listit.Composable.ButtonTonalFilled
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
             Surface (modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background){
-                Column {
+                color = MaterialTheme.colorScheme.background) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
 
+                    Text(
+                        text = "List It",
+                        style = MaterialTheme.typography.displayLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontSize = 120.sp
+                    )
 
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
+                    Spacer(modifier = Modifier.padding(100.dp))
 
-                        Text(
-                            text = "List It",
-                            style = MaterialTheme.typography.displayLarge,
-                            color = MaterialTheme.colorScheme.primary
-                        )
+                    ButtonFilled("Log In", onClick = {
+                        val intent = Intent(this@MainActivity, LogInActivity::class.java)
+                        startActivity(intent)})
 
-                        ButtonFilled("Log In") {
-                            //TODO: Add Login Logic
-                        }
-                        ButtonTonalFilled("Sign Up") {
-                            val Intent = Intent(this@MainActivity, SignUpActivity::class.java)
-                            startActivity(Intent)
-                        }
+                    Spacer(modifier = Modifier.padding(5.dp))
+
+                    ButtonTonalFilled("Sign Up") {
+                        val Intent = Intent(this@MainActivity, SignUpActivity::class.java)
+                        startActivity(Intent)
                     }
                 }
             }
@@ -56,47 +61,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun ButtonFilled(label: String,onClick: () -> Unit) {
-    Button(modifier = Modifier.padding(bottom = 5.dp, top = 200.dp), onClick = { onClick() }) {
-        Text(label,fontSize = 25.sp)
-    }
-}
-
-@Composable
-fun ButtonTonalFilled(label:String,onClick: () -> Unit) {
-    FilledTonalButton(modifier = Modifier.padding(5.dp),onClick = { onClick() }) {
-        Text(label,fontSize = 25.sp)
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Surface (modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background){
-        Column {
+        color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
 
+            Text(
+                text = "List It",
+                style = MaterialTheme.typography.displayLarge,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 120.sp
+            )
 
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
+            Spacer(modifier = Modifier.padding(100.dp))
 
-                Text(
-                    text = "List It",
-                    style = MaterialTheme.typography.displayLarge,
-                    color = MaterialTheme.colorScheme.primary,
-                    fontSize = 100.sp
-                )
-
-                ButtonFilled("Log In") {
-                    //TODO: Add Login Logic
-                }
-                ButtonTonalFilled("Sign Up") {
-                    //TODO: Add Sign In Logic
-                }
+            ButtonFilled("Log In") {
+            }
+            Spacer(modifier = Modifier.padding(5.dp))
+            ButtonTonalFilled("Sign Up") {
+                //TODO: Add Sign In Logic
             }
         }
     }
