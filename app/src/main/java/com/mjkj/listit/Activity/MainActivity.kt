35629899f079da714b82510420c037e1dc7d578e
Marcin Kawa <lerukcja2002@gmail.com>
2024-03-01 20:57:51 +0100

    working Sign Up activity

--- app/src/main/java/com/mjkj/listit/Activity/MainActivity.kt
+++ app/src/main/java/com/mjkj/listit/Activity/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import android.content.Intent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +46,8 @@ class MainActivity : ComponentActivity() {
                             //TODO: Add Login Logic
                         }
                         ButtonTonalFilled("Sign Up") {
-                            //TODO: Add Sign In Logic
+                            val Intent = Intent(this@MainActivity, SignUpActivity::class.java)
+                            startActivity(Intent)
                         }
                     }
                 }
---
+++ app/src/main/java/com/mjkj/listit/Activity/SignUpActivity.kt
@@ -0,0 +1,48 @@
+package com.mjkj.listit.Activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class SignUpActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Surface (modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background){
+                Column {
+
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+
+                        Text(
+                            text = "List It",
+                            style = MaterialTheme.typography.displayLarge,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
