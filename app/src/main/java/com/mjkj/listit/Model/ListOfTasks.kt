017aa1d6ad38cc40876c819e05517622ce1c47df
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-03-03 22:50:09 +0100

    add a basic ListOfTasks class

---
+++ app/src/main/java/com/mjkj/listit/Model/ListOfTasks.kt
@@ -0,0 +1,89 @@
+package com.mjkj.listit.Model
+
+class ListOfTasks(
+    /**
+     * The name of the list
+     */
+    private var listName: String,
+    /**
+     * The code of the list
+     */
+    private var code: String,
+    /**
+     * The tasks of the list
+     */
+    private var tasks: MutableList<Task>?,
+    /**
+     * The members of the list
+     */
+    private var members: MutableList<String>?,
+    /**
+     * The color of the list
+     */
+    private var color: String,
+    /**
+     * The description of the list
+     */
+    private var description: String?,
+    /**
+     * The creator of the list
+     */
+    private var creator: User?
+) {
+
+    fun getListName(): String {
+        return listName
+    }
+
+    fun setListName(listName: String) {
+        this.listName = listName
+    }
+
+    fun getCode(): String {
+        return code
+    }
+
+    fun setCode(code: String) {
+        this.code = code
+    }
+
+    fun getTasks(): MutableList<Task>? {
+        return tasks
+    }
+
+    fun setTasks(tasks: MutableList<Task>?) {
+        this.tasks = tasks
+    }
+
+    fun getMembers(): MutableList<String>? {
+        return members
+    }
+
+    fun setMembers(members: MutableList<String>?) {
+        this.members = members
+    }
+
+    fun getColor(): String {
+        return color
+    }
+
+    fun setColor(color: String) {
+        this.color = color
+    }
+
+    fun getDescription(): String? {
+        return description
+    }
+
+    fun setDescription(description: String?) {
+        this.description = description
+    }
+
+    fun getCreator(): User? {
+        return creator
+    }
+
+    fun setCreator(creator: User?) {
+        this.creator = creator
+    }
+}
