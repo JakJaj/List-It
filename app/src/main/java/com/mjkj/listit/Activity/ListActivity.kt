bcf201f3354673d90e19509c37e53d20b7f5ba12
Marcin Kawa <lerukcja2002@gmail.com>
2024-03-16 14:19:11 +0100

    fixed list activity

--- app/src/main/java/com/mjkj/listit/Activity/ListActivity.kt
+++ app/src/main/java/com/mjkj/listit/Activity/ListActivity.kt
@@ -1,39 +1,60 @@
 package com.mjkj.listit.Activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mjkj.listit.Composable.ListAppBar
 
 class ListActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val listCode: String = intent.getStringExtra("listCode").toString()
         setContent {
+            val listOfLists = mutableStateListOf<MutableList<String>>()
             Surface(
-                color = MaterialTheme.colorScheme.background,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background)
+            {
+                Scaffold(
+                    topBar = {
+                        ListAppBar(
+                            activity = "ListActivity",
+                            this,
+                            listOfLists
+                        )
+                    }
                 ) {
-                    Text(
-                        text = listCode,
-                        fontSize = 24.sp,
-                        textAlign = TextAlign.Center,
-                    )
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = listCode,
+                            textAlign = TextAlign.Center,
+                            color = Color.Gray,
+                            fontSize = 25.sp,
+                            modifier = Modifier.padding(15.dp)
+                        )
+                    }
                 }
             }
         }
+
     }
 }
